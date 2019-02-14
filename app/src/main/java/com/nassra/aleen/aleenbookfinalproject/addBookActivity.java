341ef7fb918308373812ff64f3ddfaeab34705c0
aleen <alona.nassra118@gmail.com>
2019-02-14 14:52:30 +0200

    aa

---
+++ app/src/main/java/com/nassra/aleen/aleenbookfinalproject/addBookActivity.java
@@ -0,0 +1,13 @@
+package com.nassra.aleen.aleenbookfinalproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class addBookActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_book);
+    }
+}
--- app/src/main/java/com/nassra/aleen/aleenbookfinalproject/bookFragment/MyItemRecyclerViewAdapter.java
+++ app/src/main/java/com/nassra/aleen/aleenbookfinalproject/bookFragment/MyItemRecyclerViewAdapter.java
@@ -6,6 +6,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.nassra.aleen.aleenbookfinalproject.R;
 import com.nassra.aleen.aleenbookfinalproject.bookFragment.ReadBookFragment.OnListFragmentInteractionListener;
 import com.nassra.aleen.aleenbookfinalproject.bookFragment.dummy.DummyContent.DummyItem;
 
