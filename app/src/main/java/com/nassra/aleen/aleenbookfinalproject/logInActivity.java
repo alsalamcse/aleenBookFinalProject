8f524cb9ec560de5da7e6ee7a7ea3791828551ee
aleen <alona.nassra118@gmail.com>
2019-02-17 12:21:40 +0200

    aa

--- app/src/main/java/com/nassra/aleen/aleenbookfinalproject/logInActivity.java
+++ app/src/main/java/com/nassra/aleen/aleenbookfinalproject/logInActivity.java
@@ -1,15 +1,26 @@
 package com.nassra.aleen.aleenbookfinalproject;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.nassra.aleen.aleenbookfinalproject.bookFragment.ProfileFragment;
 
 public class logInActivity extends AppCompatActivity {
     private EditText etEmail,etPassword;
     private Button btnSignUp,btnSignIn;
+    private FirebaseAuth auth;
+    private FirebaseUser user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,8 +33,9 @@ public class logInActivity extends AppCompatActivity {
         btnSignIn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent i=new Intent(logInActivity.this,StartActivity.class);
-                startActivity(i);
+                //Intent i=new Intent(logInActivity.this,ProfileFragment.class);
+                //startActivity(i);
+                dataHandler();
 
             }
         });
@@ -34,5 +46,50 @@ public class logInActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+        auth=FirebaseAuth.getInstance();
+        user=auth.getCurrentUser();
     }
+
+    private void dataHandler() {
+        boolean isok=true;
+        String email=etEmail.getText().toString();
+        String pass=etPassword.getText().toString();
+        if (email.length()<4||email.indexOf('@')<0||email.indexOf('.')<0){
+            etEmail.setError("wrong email");
+            isok=false;
+
+        }
+        if ((pass.length())<8){
+            etPassword.setError("wrong password");
+            isok=false;
+        }
+        if (isok){
+            signIn(email,pass);
+
+        }
+    }
+    private void signIn (String Email,String pass){
+        auth.signInWithEmailAndPassword(Email,pass).addOnCompleteListener(logInActivity.this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()){
+                    Toast.makeText(logInActivity.this, "sign in seccessful", Toast.LENGTH_SHORT).show();
+                    finish();
+                    Intent intent=new Intent(logInActivity.this,MainTabsActivity.class);
+                    startActivity(intent);
+
+                }
+                else {
+                    Toast.makeText(logInActivity.this, ""+task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                    task.getException().printStackTrace();
+
+                }
+
+            }
+
+
+
+        });
+    }
+
 }
