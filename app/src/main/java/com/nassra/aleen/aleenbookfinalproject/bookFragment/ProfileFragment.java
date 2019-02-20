eab0ad590cba1b271665516c8790455e7fb4ce1d
aleen <alona.nassra118@gmail.com>
2019-02-20 09:41:17 +0200

    aa

--- app/src/main/java/com/nassra/aleen/aleenbookfinalproject/bookFragment/ProfileFragment.java
+++ app/src/main/java/com/nassra/aleen/aleenbookfinalproject/bookFragment/ProfileFragment.java
@@ -1,17 +1,20 @@
 package com.nassra.aleen.aleenbookfinalproject.bookFragment;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.service.autofill.TextValueSanitizer;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.nassra.aleen.aleenbookfinalproject.R;
+import com.nassra.aleen.aleenbookfinalproject.bookListActivity;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -20,6 +23,7 @@ public class ProfileFragment extends Fragment {
     private TextView tvProfile;
     private ImageView imPic;
     private EditText etName,etFamily,etPhone,etAge;
+    private Button btnList;
 
 
     public ProfileFragment() {
@@ -34,12 +38,29 @@ public class ProfileFragment extends Fragment {
     {
         View view=inflater.inflate(R.layout.fragment_profile, container, false);
         tvProfile=view.findViewById(R.id.tvProfile);
+        imPic=view.findViewById(R.id.imPic);
+        etName=view.findViewById(R.id.etName);
+        etFamily=view.findViewById(R.id.etFamily);
+        etPhone=view.findViewById(R.id.etPhone);
+        etAge=view.findViewById(R.id.etAge);
+        btnList=view.findViewById(R.id.btnList);
+        btnList.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getActivity(),bookListActivity.class);
+                startActivity(i);
+            }
+        });
+
 
 
         // Inflate the layout for this fragment
         return view;
 
     }
+
+
+
 
 
 
